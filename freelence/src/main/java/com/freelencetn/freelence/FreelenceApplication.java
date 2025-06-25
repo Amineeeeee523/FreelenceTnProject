757b69a6c5604237e7a61e1bada301d1a5c95a92
aminejemli <aminejemli0@gmail.com>
2025-06-25 21:21:17 +0100

    jjj

---
+++ freelence/src/main/java/com/freelencetn/freelence/FreelenceApplication.java
@@ -0,0 +1,13 @@
+package com.freelencetn.freelence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelenceApplication.class, args);
+	}
+
+}
